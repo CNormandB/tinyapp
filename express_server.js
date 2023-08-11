1e0b62498137d7f656164024180c320d0da3887a
Casandra Normand <casandra.normand@outlook.com>
2023-08-10 22:30:37 -0400

    Used cookie-parser to add username when logged in

--- express_server.js
+++ express_server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const  cookieParser = require('cookie-parser')
 const app = express();
+
 const PORT = 8080; // default port 8080
+
+app.use(cookieParser())
 
 app.set("view engine", "ejs");
 
@@ -27,12 +31,17 @@ app.use(express.urlencoded({ extended: true }));
 
 //render "Create New URL" page with urls_new.ejs
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+  username: req.cookies["username"]
+};
+  res.render("urls_new", templateVars);
 });
 
 //render "My URL's" page using the urlDatabase object and urls_index.ejs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
@@ -45,7 +54,9 @@ app.post("/urls", (req, res) => {
 
 //render individual pages for each  ID (access by adding the short URL after /urls/)
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
+  const templateVars = { 
+    username: req.cookies["username"],
+    id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
@@ -79,6 +90,7 @@ app.post("/login",(req, res) => {
   res.cookie("username", req.body.username);
   res.redirect("/urls");
 });
+
 
 //say hello on / page
 app.get("/", (req, res) => {
